79211065c88f3e0dda4c1adf18bde687fc684888
unknown <C:\Users\Marina\AppData\Roaming\The Bat!>
2022-12-02 23:09:39 +0800

    написали 1 задание

--- hw 2/task 1/Program.cs
+++ hw 2/task 1/Program.cs
@@ -1,2 +1,7 @@
-﻿// See https://aka.ms/new-console-template for more information
+﻿// Задача 10 Напишите программу, которая принимает на вход трёхзначное число и 
+на выходе показывает вторую цифру этого числа.
+
+456 -> 5
+782 -> 8
+918 -> 1//
 Console.WriteLine("Hello, 2 World!");
